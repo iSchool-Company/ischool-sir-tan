0d4c2ff463becc4b6e375d60d05a75ec38125a6f
Jamel Cagampang <36389770+jamcagampang@users.noreply.github.com>
2021-04-16 21:35:15 +0800

    Fixed Date Problem

--- database/classroom/search.php
+++ database/classroom/search.php
@@ -14,7 +14,7 @@ if (
   $searchFullText = '+' . str_replace(' ', ' +', $search) . '*';
   $classrooms = [];
 
-  $command = "SELECT cr.id, CONCAT(u.first_name, ' ', u.middle_name, ' ', u.last_name), u.profile_picture, cr.class, cr.subject FROM classrooms AS cr LEFT JOIN users AS u ON cr.teacher_id = u.id LEFT JOIN classroom_student_designation AS csd ON cr.id = csd.classroom_id AND csd.student_id = ? WHERE csd.id IS NULL AND cr.is_deleted = 0 AND cr.date_end + 1 > now() AND (MATCH(u.first_name, u.middle_name, u.last_name) AGAINST('$searchFullText' IN BOOLEAN MODE) OR MATCH(cr.class, cr.subject) AGAINST('$searchFullText' IN BOOLEAN MODE)) LIMIT 5";
+  $command = "SELECT cr.id, CONCAT(u.first_name, ' ', u.middle_name, ' ', u.last_name), u.profile_picture, cr.class, cr.subject FROM classrooms AS cr LEFT JOIN users AS u ON cr.teacher_id = u.id LEFT JOIN classroom_student_designation AS csd ON cr.id = csd.classroom_id AND csd.student_id = ? WHERE csd.id IS NULL AND cr.is_deleted = 0 AND cr.date_end > now() AND (MATCH(u.first_name, u.middle_name, u.last_name) AGAINST('$searchFullText' IN BOOLEAN MODE) OR MATCH(cr.class, cr.subject) AGAINST('$searchFullText' IN BOOLEAN MODE)) LIMIT 5";
 
   $statement = $connection->prepare($command);
   $statement->bind_param('i', $userId);
