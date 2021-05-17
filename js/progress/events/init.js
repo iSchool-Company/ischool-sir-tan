a88015fa874b8744ff53c6e4438cd1a9f0294329
Jamel Cagampang <36389770+jamcagampang@users.noreply.github.com>
2021-05-17 17:14:53 +0800

    Bug fix

--- js/progress/events/init.js
+++ js/progress/events/init.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
         window.open('classroom_summarized_material_report.php?classroom_id=' + classroomId);
         break;
       case 'detailed':
-        materialId = $('#report_form [name="materials"]').val();
+        materialId = $('#feedback_form [name="materials"]').val();
         window.open('classroom_feedback_report.php?material_id=' + materialId);
         break;
     }
